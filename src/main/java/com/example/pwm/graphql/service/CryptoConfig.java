0606976a0ef80fcbfe0baa40c6eaac7b86f6f101
Fabian <fabian.wiedenmeyer@gmail.com>
2025-10-21 16:28:00 +0200

    add CryptoConfig

---
+++ src/main/java/com/example/pwm/graphql/service/CryptoConfig.java
@@ -0,0 +1,15 @@
+package com.example.pwm.graphql.service;
+
+import com.example.pwm.graphql.service.CryptoService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CryptoConfig {
+
+    @Bean
+    public CryptoService cryptoService(@Value("${crypto.master}") String masterB64) {
+        return new CryptoService(masterB64);
+    }
+}
